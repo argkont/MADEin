f0564c1de0f8ddfa9988a68936b2da181a7c1fde
argkont <kohacaelo@gmail.com>
2013-05-13 00:27:13 -0400

    made in commit

---
+++ soon_page/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Desarrollo MADE IN Sociable</title>
+	
+	<link rel="stylesheet/less" type="text/css" href="css/sty_soon.less"/>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css"/>
+	
+</head>
+
+<body id="" onload="">
+	
+	<div class="container-fluid">
+			<div class="container">
+				
+				<div class="row-fluid">
+					
+					<div class="span12 text-center well-large">
+						
+						<img src="img/logo.gif"/>
+						
+					</div>
+					
+				</div>
+				<div class="container">
+					<div class="row-fluid">
+							
+						<!-- Begin MailChimp Signup Form -->
+							<form action="http://adnetwork.us7.list-manage2.com/subscribe/post?u=c804ae5d6693b8b1cbadf6dcb&amp;id=1978f51f39" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							
+								<div class="span6 offset4">
+									
+									<input type="email" value="" name="EMAIL" class="required email pull-left" id="mce-EMAIL">
+							
+									<input type="submit" value="Cuentame!" name="subscribe" id="mc-embedded-subscribe" class="button pull-left">
+								
+								</div>
+							
+									<div class="response" id="mce-error-response" style="display:none"></div>
+									<div class="response" id="mce-success-response" style="display:none"></div>
+									
+								</form>
+						<!--End mc_embed_signup-->
+						
+					</div>
+				</div>		
+					
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="https://raw.github.com/cloudhead/less.js/master/dist/less-1.3.3.min.js"></script>
+</body>
+</html>
